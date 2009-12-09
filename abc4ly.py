4ab089dd59b56c03261e6bcdbd4bb94a356575b0
gwen <gwen@localhost>
2009-12-09 18:06:56 +0100

    Analyse des champs informatifs T, C et R.

--- abc4ly.py
+++ abc4ly.py
@@ -4,20 +4,29 @@
 import string
 
 title = ""
+composer = ""
+rythm = ""
 
 def process_info_line(line):
+    raw_field = line[2:] # Remove the leading "T:" or so
+    # Remove leading/trailing spaces, and substititue any occurence
+    # of more than one space by just one space
+    nice_field = string.join(raw_field.split(), " ")
     if line[0] == 'T':
-        # Remove the leading "T:"
-        tmp = line[2:]
-        # Remove leading/trailing spaces, and substititue any occurence
-        # of more than one space by just one space
-        tmp = string.join(tmp.split(), " ")
         global title
-        title = tmp
+        title = nice_field
+    elif line[0] == 'C':
+        global composer
+        composer = nice_field
+    elif line[0] == 'R':
+        global rythm
+        rythm = nice_field
 
 def process_line(line):
     if line[0] in string.ascii_uppercase and line[1] == ":":
         process_info_line(line)
+    # Comments (lines starting with '%') and empty lines are silently
+    # ignored
 
 def convert(abc_filename, ly_filename):
     with open(abc_filename, 'r') as abc_file:
--- testabc4ly.py
+++ testabc4ly.py
@@ -22,5 +22,29 @@ class TestInformationFields(unittest.TestCase):
         abc4ly.convert("regression/header.abc", "")
         self.assertEqual(abc4ly.title, "Hello, world!")
 
+    def test_composer(self):
+        abc4ly.convert("regression/header.abc", "")
+        self.assertEqual(abc4ly.composer, "M. Foo")
+
+    def test_rythm(self):
+        abc4ly.convert("regression/header.abc", "")
+        self.assertEqual(abc4ly.rythm, "reel")
+
+    def test_other(self):
+        abc4ly.convert("regression/header.abc", "")
+
+class TestMisc(unittest.TestCase):
+
+    def test_no_ending_empty_line(self):
+        abc4ly.convert("regression/header_no_endl.abc", "")
+        self.assertEqual(abc4ly.title, "Hello, world!")
+
+    def test_comments(self):
+        abc4ly.convert("regression/header_with_comments.abc", "")
+
+    def test_blank_lines(self):
+        # A mix of empty lines, spaces and tabs
+        abc4ly.convert("regression/header_with_blank_lines.abc", "")
+
 if __name__ == '__main__':
     unittest.main()
